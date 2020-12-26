7d7e782ff01c400f18c2b6270d2d4d1ddd006742
chenxiaohao <>
2020-12-26 16:43:28 +0800

    first commit

---
+++ Example/Pods/Target Support Files/RTools/RTools-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RTools : NSObject
+@end
+@implementation PodsDummy_RTools
+@end
---
+++ RTools/Classes/RTools.swift
@@ -0,0 +1,16 @@
+//
+//  CXHTools.swift
+//  TospurB
+//
+//  Created by Rick on 2020/12/26.
+//  Copyright © 2020 tospur co,.ltd. All rights reserved.
+//
+
+import UIKit
+
+class CXHTools: NSObject {
+
+    func test() {
+        print("hello world!")
+    }
+}
